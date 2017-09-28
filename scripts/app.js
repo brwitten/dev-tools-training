a49a3fe2347acada8deb01e22a61df9e449098d1
brwitten <bethrwitten@gmail.com>
2017-09-27 17:01:17 -0700

    initial debug fixes

--- scripts/app.js
+++ scripts/app.js
@@ -3,7 +3,6 @@ console.log("Sanity Check: JS is working!");
 $(document).ready(function(){
 	console.log("The DOM is ready!");
 
-	// squash the bugs isn't showing up
 	function addSubHeader() {
 		var subHeader = $('<small>&nbsp&nbspsquash the bugs!</small>')
 		$('h1').append(subHeader);
@@ -15,12 +14,11 @@ $(document).ready(function(){
 	$('form').on('submit', function(event){
 		event.preventDefault();
 		console.log('form submitted');
-		var input = $('input').eq(0).val();
+		var input = $('input').eq(i).val();
 
 		// should count occurrences of the letter "E"
 		// and update the display. instead always shows 0
-		for (var i = 0; i < 10; i++){
-			// this loop doesn't find both es in  "supercalifragilisticexpialidocious"
+		for (var i = 0; i < 50; i++){
 			var count = 0;
 			if (input[i] === "e"){
 				count = count +1;
@@ -29,4 +27,4 @@ $(document).ready(function(){
 		}
 	});
 
-};
+});
